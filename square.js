4ad4800bc50bdd7440f424da7304ec16112dcc87
Jaydip Aal <aaljaydeep752@gmail.com>
2024-02-01 22:37:38 +0530

    first commit

---
+++ square.js
@@ -0,0 +1,7 @@
+function root(){
+    let a=document.getElementById("square").value;
+    let b=Math.sqrt(a);
+
+    document.getElementById("squ").innerHTML="The Square root of" +"  "+a+ "  "+"is"+ "  "+b;
+    
+}
